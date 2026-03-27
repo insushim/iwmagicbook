5540ebaaa2f7ebfb5d40639ed757b2f86c902c8e
insushim <insushim@users.noreply.github.com>
2026-03-27 22:43:30 +0900

    Fix: Lumi size/position, remove broken unicode icons

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1774618756|10497355';
+const CACHE_VERSION = '1774618992|10570025';
 /** @type {string} */
 const CACHE_PREFIX = 'SpellBook Chroni-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
