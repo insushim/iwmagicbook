dbc1f4a4f45e67e6354cd8ccfd3272642337fa6a
insushim <insushim@users.noreply.github.com>
2026-03-27 22:39:43 +0900

    Update: fullscreen canvas + redesigned grade select UI

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1774531829|6062170';
+const CACHE_VERSION = '1774618756|10497355';
 /** @type {string} */
 const CACHE_PREFIX = 'SpellBook Chroni-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
