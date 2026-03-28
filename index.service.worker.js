0fe97ff0f9eda02741b937b025f760387d8f3b91
insushim <insushim@users.noreply.github.com>
2026-03-28 13:05:06 +0900

    Visual overhaul: HUD styling, bg fix, sprite cleanup, bounds fix

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1774669980|6210752';
+const CACHE_VERSION = '1774670692|7242275';
 /** @type {string} */
 const CACHE_PREFIX = 'SpellBook Chroni-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
